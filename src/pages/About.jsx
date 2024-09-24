a7da1c681c3534699b5ac0af4a9d8581c2dd4d22
aniraannu <51684910+aniraannu@users.noreply.github.com>
2024-09-23 17:28:29 -0700

    Made some changes in about description and changed the image in about section

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -16,7 +16,24 @@ function About() {
         <div className="about__data">
           <img src={AboutImg} alt="" className="about__img" />
           <p className="about__description">
-            Anira Raveendran is a passionate and dedicated software engineer.
+            Hello! I’m a passionate software developer with a diverse background
+            in software testing and engineering. With over 6 years of experience
+            in software testing—spanning manual and automation techniques—I have
+            a deep understanding of the software development lifecycle (SDLC)
+            across various methodologies, including Waterfall, V-Model, and
+            Agile. Having recently completed a Full Stack Development Bootcamp
+            at UC Berkeley Extension, I am excited to transition into a software
+            development role. My technical skills include building full-stack
+            applications using Node.js, React, Express, MongoDB, and GraphQL. I
+            am dedicated to crafting efficient, user-friendly solutions that
+            solve real-world problems. My academic foundation in Civil
+            Engineering, complemented by a Master’s in Structural Engineering,
+            equips me with a unique analytical perspective and strong
+            problem-solving abilities. I thrive on collaboration and am eager to
+            contribute my diverse skill set to a dynamic and innovative team. I
+            look forward to connecting with forward-thinking organizations where
+            I can leverage my experience and enthusiasm for software
+            development!
           </p>
 
           {/* <div className="about__info">
