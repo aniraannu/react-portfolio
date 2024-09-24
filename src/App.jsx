25684f76e0eebb89b617f04d84c55c909fe627c8
aniraannu <51684910+aniraannu@users.noreply.github.com>
2024-09-24 09:14:52 -0700

    Made changes in App.jsx to import all the components and pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { Fragment } from "react";
+import Navbar from "./components/navbar/Navbar";
+import "./App.css";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Skills from "./pages/Skills";
+import Contact from "./pages/Contact";
+import Footer from "./components/footer/Footer";
+import ScrollTop from "./components/scroll/ScrollTop";
+import Porfolio from "./pages/Porfolio";
+// import { Route, Switch, BrowserRouter } from "react-router-dom";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Fragment>
+      <main className="main">
+        <Navbar />
+        <Home />
+        <About />
+        <Skills />
+        <Porfolio />
+        <Contact />
+      </main>
+      <Footer />
+      <ScrollTop />
+    </Fragment>
+  );
 }
 
-export default App
+export default App;
