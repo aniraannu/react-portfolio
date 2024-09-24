f0d663826939c2f36b3fd642a9a8adc18daea57d
aniraannu <51684910+aniraannu@users.noreply.github.com>
2024-09-24 09:03:52 -0700

    Minor changes in the main.jsx

---
+++ src/components/sections/Portfolio/Development/Development.jsx
@@ -0,0 +1,28 @@
+import { Fragment } from "react";
+import { Zoom } from "@mui/material";
+import Card from "../../../card/Card.jsx";
+import { developmentFiles } from "./development-files.js";
+
+function Development() {
+  return (
+    <Fragment>
+      <div className="portfolio__sections">
+        {developmentFiles.map((website, index) => (
+          <Zoom in={true} timeout={700} key={index}>
+            <a href={website.route} target="_blank" rel="noreferrer">
+              <Card
+                key={website.description}
+                hexa={website.hexa}
+                title={website.title}
+                description={website.description}
+                image={website.image}
+              />
+            </a>
+          </Zoom>
+        ))}
+      </div>
+    </Fragment>
+  );
+}
+
+export default Development;
